cc71477a84b425417ff71c69a8c15f692cb5cca2
root <root@Ubuntu.myguest.virtualbox.org>
2025-11-20 19:59:03 +0530

    2nd commit

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,35 +1,27 @@
-#!/bin/sh
-#Assignment1
-
-if [ $# -lt 2 ]
+#!/bin/bash
+set -xev
+if [ $# -lt 1 ]
 then
-        echo "usage ./script.sh directory string_name"
+	echo "filesdir does not represent a directory on the filesysteM"
         exit 1
-fi
-
-
-filedir=$1
-str=$2
-fil_no=0
-match=0
-match_no=0
-
-cd $filedir
-if [ $? -ne 0 ]
+elif [ $# -lt 2 ]
 then
-      echo "file does not exist\n"
-      exit 1
+	echo "filesdir and searchstr were not specified"
+	exit 1
+else
+	if [ -d $1 ]
+	then
+	       	grep -r $2 $1
+    		if [ $? -eq 0 ]
+     		then
+			number_of_files=$(find  $1  -type f | wc -l)
+			number_of_matching_lines=$(find $1 -type f -print0 | xargs -0  grep $2 | wc -l)
+			echo "The number of files are ${number_of_files} and the number of matching lines are ${number_of_matching_lines}"
+		else
+			echo "error"
+			exit 1
+		fi
+	else
+		exit 1
+	fi
 fi
-
-pwd
-
-for file in $(find . -type f ); do
-    match=$(grep -c "$str" "$file" 2>/dev/null || echO 0 ) 
-    if  [ $match -gt 0 ]
-    then
-       fil_no=$((fil_no + 1))
-    fi
-    match_no=$((match_no + match))
-done
-
-echo "No of files is $fil_no and no. of matches is $match_no" 
